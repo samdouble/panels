6257926ef8b973885fd74e5b103a0b143686e743
samdouble <samleafs@protonmail.com>
2025-07-09 05:56:07 -0400

    chore: upgrade packages to fix vulnerabilities

--- Panels/Slot.cs
+++ Panels/Slot.cs
@@ -20,8 +20,8 @@ namespace Panels
             this.parent = parent;
             List<XmlNode> xmlPanels = new List<XmlNode>(xmlSlot.ChildNodes.Cast<XmlNode>());
             this.panels.AddRange(xmlPanels.Select(xmlPanel => new Panel(parent, xmlPanel)));
-            this.paddingMaxGauchePct = xmlSlot.Attributes["maxCropLeft"] != null ? float.Parse(xmlSlot.Attributes["maxCropLeft"].InnerText) : 0f;
-            this.paddingMaxDroitePct = xmlSlot.Attributes["maxCropRight"] != null ? float.Parse(xmlSlot.Attributes["maxCropRight"].InnerText) : 0f;
+            this.paddingMaxGauchePct = xmlSlot?.Attributes["maxCropLeft"] != null ? float.Parse(xmlSlot.Attributes["maxCropLeft"].InnerText) : 0f;
+            this.paddingMaxDroitePct = xmlSlot?.Attributes["maxCropRight"] != null ? float.Parse(xmlSlot.Attributes["maxCropRight"].InnerText) : 0f;
             this.paddingGauche = 0f;
             this.paddingDroite = 0f;
         }
